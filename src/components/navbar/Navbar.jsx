465c5cbb2199b00d40d07581cdc044a49ae90035
Alexander Kharmansky <davilex_x@vk.com>
2023-05-10 17:41:59 +0600

    feat: start navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/svg/logo.svg';
 import './navbar.css';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <ul className="gpt3__navbar-list">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#wgpt3">What is GPT3?</a>
+            </li>
+            <li>
+              <a href="#possibility">Open AI</a>
+            </li>
+            <li>
+              <a href="#features">Case Studies</a>
+            </li>
+            <li>
+              <a href="#blog">Library</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
