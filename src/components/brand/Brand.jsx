b6ca7b6830d5f7430353779dc20f12bc002f97ea
Alexander Kharmansky <davilex_x@vk.com>
2023-05-15 16:55:33 +0600

    feat: finish brand section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import './brand.css';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 
 function Brand() {
-  return <div>Brand</div>;
+  return (
+    <section className="brand">
+      <div className="brand__container">
+        <div className="brand__logo">
+          <img src={google} alt="google" className="brand__img" />
+        </div>
+        <div className="brand__logo">
+          <img src={slack} alt="slack" className="brand__img" />
+        </div>
+        <div className="brand__logo">
+          <img src={atlassian} alt="atlassian" className="brand__img" />
+        </div>
+        <div className="brand__logo">
+          <img src={dropbox} alt="dropbox" className="brand__img" />
+        </div>
+        <div className="brand__logo">
+          <img src={shopify} alt="shopify" className="brand__img" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Brand;
