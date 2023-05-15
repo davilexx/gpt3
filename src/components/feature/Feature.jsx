6de5be1d16e153f4b523f7f8c423f996c3a4f866
Alexander Kharmansky <davilex_x@vk.com>
2023-05-15 18:27:07 +0600

    feat: finish whatgpt3 section for desktop

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './feature.css';
 
-function Feature() {
-  return <div>Feature</div>;
-}
+const Feature = ({ title, text }) => {
+  return (
+    <div className="wgpt3__feature feature">
+      <div className="feature__container">
+        <div className="feature__top">
+          <div className="feature__gradient gradient__bar"></div>
+          <h3 className="feature__title">{title}</h3>
+        </div>
+        <p className="feature__description">{text}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Feature;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
 import './whatGPT3.css';
+import { Feature } from '../../components';
 
 function WhatGPT3() {
-  return <div>WhatGPT3</div>;
+  return (
+    <section className="wgpt3">
+      <div className="wgpt3__container">
+        <div className="wgpt3__first-feature">
+          <Feature
+            title="What is GPT-3"
+            text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+          />
+        </div>
+        <div className="wgpt3__headline">
+          <h2 className="wgpt3__title gradient__text">
+            The possibilities are beyond your imagination
+          </h2>
+          <p className="wgpt3__description">Explore The Library</p>
+        </div>
+        <div className="wgpt3__features">
+          <Feature
+            title="Chatbots"
+            text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "
+          />
+          <Feature
+            title="Knowledgebase"
+            text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+          />
+          <Feature
+            title="Education"
+            text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default WhatGPT3;
