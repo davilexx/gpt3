61fcb3c650c0f5badef4105c0d8fc4d9c4de2715
Alexander Kharmansky <davilex_x@vk.com>
2023-05-16 16:39:17 +0600

    feat: finish cta section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import './cta.css';
 
 function CTA() {
-  return <div>CTA</div>;
+  return (
+    <section className="cta">
+      <div className="cta__container">
+        <div className="cta__wrapper">
+          <div className="cta__left">
+            <p className="cta__description">
+              Request Early Access to Get Started
+            </p>
+            <h2 className="cta__title">
+              Register today & start exploring the endless possibilities.
+            </h2>
+          </div>
+          <button className="cta__btn">Get Started</button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default CTA;
